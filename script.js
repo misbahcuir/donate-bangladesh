8d720ba19ed91ee1f556bfbdfd49a36fee3281ba
misbah3700 <misbah3700@outlook.com>
2024-09-25 10:49:20 +0600

    Style Completed

---
+++ script.js
@@ -0,0 +1,10 @@
+// Functions to get Element by ID
+
+function getTextByID (id){
+    let text = document.getElementById('id').innerText;
+    return text;
+}
+function getValueByID (id){
+    let value = document.getElementById('id').value;
+    return value;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'white': '#ffffff',
+            'main': '#b4f461',
+            'midnight': '#121063',
+            'metal': '#565584',
+            'tahiti': '#3ab7bf',
+            'silver': '#ecebff',
+            'bubble-gum': '#ff77e9',
+            'bermuda': '#78dcca',
+          },
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'main': '#b4f461',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
+    },
+  }
+  
